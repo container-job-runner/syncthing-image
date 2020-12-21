deff641a770ff05059b7d3b56a097e577c3bfe81
buvoli <tommasosignup@gmail.com>
2020-12-21 02:51:58 +0000

    Added environment variable SYNCTHING_GUI_PORT

--- start.sh
+++ start.sh
@@ -31,6 +31,7 @@ fi
 # -- configure ports -----------------------------------------------------------
 sed -i "s/{{ SYNCTHING_LISTEN_PORT }}/$SYNCTHING_LISTEN_PORT/g" ~/.config/syncthing/config.xml
 sed -i "s/{{ SYNCTHING_CONNECT_PORT }}/$SYNCTHING_CONNECT_PORT/g" ~/.config/syncthing/config.xml
+sed -i "s/{{ SYNCTHING_GUI_PORT }}/$SYNCTHING_GUI_PORT/g" ~/.config/syncthing/config.xml
 
 # -- start syncthing -----------------------------------------------------------
 syncthing -no-browser
