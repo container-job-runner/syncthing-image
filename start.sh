f37baa988e6e1b49f9e6e83c4aff65b5642f0177
buvoli <tommasosignup@gmail.com>
2021-01-21 06:53:21 +0000

    Added optional environment variables for tuning file scanning.

--- start.sh
+++ start.sh
@@ -32,6 +32,8 @@ fi
 sed -i "s/{{ SYNCTHING_LISTEN_PORT }}/$SYNCTHING_LISTEN_PORT/g" ~/.config/syncthing/config.xml
 sed -i "s/{{ SYNCTHING_CONNECT_PORT }}/$SYNCTHING_CONNECT_PORT/g" ~/.config/syncthing/config.xml
 sed -i "s/{{ SYNCTHING_GUI_PORT }}/$SYNCTHING_GUI_PORT/g" ~/.config/syncthing/config.xml
+sed -i "s/{{ SYNCTHING_FS_WATCHER_DELAY_S }}/$SYNCTHING_FS_WATCHER_DELAY_S/g"  ~/.config/syncthing/config.xml
+sed -i "s/{{ SYNCTHING_RESCAN_INTERVAL_S }}/$SYNCTHING_RESCAN_INTERVAL_S/g"  ~/.config/syncthing/config.xml
 sed -i "s/{{ SYNCTHING_SYNC_DIRECTORY }}/$(echo $SYNCTHING_SYNC_DIRECTORY | sed -e 's/[\/&]/\\&/g')/g" ~/.config/syncthing/config.xml
 
 # -- manually create .stfolder -------------------------------------------------
